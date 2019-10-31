45f5b357ea4d77103bf272be57d4c785955745ed
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-31 14:18:13 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ function setup() {
 	button = select('#dabutton');
 	button.style("background-color","#F6F6F6");
 	button.style("padding","2px");
-	button.position(randomLocation);
+	// button.position(randomLocation);
 	button.mousePressed(randomLocation);
 
 	nabutton = select('#nabutton');
 	nabutton.style("background-color","#3D87F9");
 	nabutton.style("padding","15px");
-	nabutton.position(randomLocation);
+	// nabutton.position(randomLocation);
 }
 
 
