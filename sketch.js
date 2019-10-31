f78e5df876247e3cb4667a831f4b26e1476ed758
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-31 12:48:15 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,50 @@
+var button;
+var x=500;
+var y=500;
+var nabutton;
 
-let button;
 
 function setup() {
 	// make canvas full screen
 	createCanvas(windowWidth, windowHeight);
-	background(0);
-	button = createButton('Deactivate Account');
-  button.position(input.x + input.width, 65);
-	button.mousePressed(changeBG);
+	// button=createButton('deactivate your account');
+	button = select('#dabutton');
+	button.style("background-color","#F6F6F6");
+	button.style("padding","2px");
+	button.position(x,y);
+	button.mousePressed(randomLocation);
+
+	nabutton = select('#nabutton');
+	nabutton.style("background-color","#3D87F9");
+	nabutton.style("padding","15px");
+	nabutton.position(center);
 }
 
-function changeBG() {
-let val = random(255);
-background(val);
+
+function randomLocation() {
+	 x=random(windowWidth)
+	 y=random(windowHeight)
+	 button.position(x,y);
 }
+
+
+
+
 
 
 
 
 
 function draw() {
-	// adding clear() to the draw loop will clear each frame, erasing object trails
-	// clear();
+	noStroke();
+	fill(255);
+	textAlign(CENTER);
+	text("ioegnoingeioseginsigenpseingopsegpnseopgnosengopegsegsgsegseg", 400, 500);
 
-	ellipse(mouseX, mouseY, 100);
+
+
+
+
+
+
 }
