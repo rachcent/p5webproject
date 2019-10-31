6ab5856022f10e89f9f97d2eb5e31c22fde0e299
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-31 14:10:38 -0700

    e

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var x=500;
 var y=500;
 
@@ -11,13 +10,13 @@ function setup() {
 	button = select('#dabutton');
 	button.style("background-color","#F6F6F6");
 	button.style("padding","2px");
-	button.position(x,y);
+	button.position(randomLocation);
 	button.mousePressed(randomLocation);
 
 	nabutton = select('#nabutton');
 	nabutton.style("background-color","#3D87F9");
 	nabutton.style("padding","15px");
-	nabutton.position(center);
+	nabutton.position(randomLocation);
 }
 
 
@@ -26,6 +25,23 @@ function randomLocation() {
 	 y=random(windowHeight)
 	 button.position(x,y);
 }
+
+
+
+
+
+
+
+
+
+function draw() {
+	noStroke();
+	fill(255);
+	textAlign(CENTER);
+	text("ioegnoingeioseginsigenpseingopsegpnseopgnosengopegsegsgsegseg", 400, 500);
+
+
+
 
 
 
